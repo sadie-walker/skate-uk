cfb5f645c917896230f3d2a400f07f713d6cc02e
Sadie Walker <64597433+sadie-walker@users.noreply.github.com>
2021-10-14 16:32:06 +0100

    add Open sans font for body text and center homepage layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 import HomeDirections from "../components/HomeDirections";
+import styles from "../styles/HomePage.module.css";
 
 export default function Home() {
 	return (
-		<section>
+		<section className={styles.home}>
 			<h1>Hi Skaters! Welcome to the home of UK skating.</h1>
 			<p>
 				Whether your looking for a new place to skate, people to skate
