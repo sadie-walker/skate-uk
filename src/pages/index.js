7a54b62cf4bfeb0d2e3179416b2528351fa6e7e0
Sadie Walker <64597433+sadie-walker@users.noreply.github.com>
2021-10-15 12:50:17 +0100

    fix text sizing & font on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,10 @@ import styles from "../styles/HomePage.module.css";
 export default function Home() {
 	return (
 		<section className={styles.home}>
-			<h1>Hi Skaters! Welcome to the home of UK skating.</h1>
+			<h1>
+				<span>Hi Skaters!</span> <br /> Welcome to the home of UK
+				skating.
+			</h1>
 			<p>
 				Whether your looking for a new place to skate, people to skate
 				with or equipment recommendations - we have it all.
