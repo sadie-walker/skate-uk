df0ef61bfbc900d5f58e60c8e1a6f2b39469bd6d
sadie-walker <64597433+sadie-walker@users.noreply.github.com>
2021-10-21 15:25:41 +0100

    initialise firebase db

---
+++ database/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyAW8ZmuuruUahF5y09D3rRE9vGQD3DNZnw",
+	authDomain: "skate-uk.firebaseapp.com",
+	databaseURL:
+		"https://skate-uk-default-rtdb.europe-west1.firebasedatabase.app",
+	projectId: "skate-uk",
+	storageBucket: "skate-uk.appspot.com",
+	messagingSenderId: "494609526793",
+	appId: "1:494609526793:web:4aab8d1e0ad400c9a308d8",
+	measurementId: "G-KP7G6FHFLZ",
+};
+
+initializeApp(firebaseConfig);
